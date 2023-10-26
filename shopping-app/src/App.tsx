87fb04b46d4a5e51d97d0a7272cc703ddb5edb15
Josephwhx <JosephWonghx@gmail.com>
2023-10-26 14:03:18 +0800

    Initialize project

---
+++ shopping-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greeter from './Component/Greeter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeter/>
+    </div>
+  );
+}
+
+export default App;
