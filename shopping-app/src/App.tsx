82c1d56a50836f64aadacc3ac8fc0c10c853ca29
Josephwhx <JosephWonghx@gmail.com>
2023-10-26 15:10:00 +0800

    include quantity field

--- shopping-app/src/App.tsx
+++ shopping-app/src/App.tsx
@@ -10,8 +10,8 @@ import './App.css';
 function App() {
   const [items, setItems] = useState<Item[]>([])
 
-  const addItem = (product: string) => {
-    setItems([...items, {id: getId(), product, quantity: 1}]);
+  const addItem = (product: string, quantity: number) => {
+    setItems([...items, {id: getId(), product, quantity}]);
   }
 
   // const items = [
