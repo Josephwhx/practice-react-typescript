8e29ecded3125341b0a985a9326fd9af94ec999b
Josephwhx <JosephWonghx@gmail.com>
2023-10-26 14:26:23 +0800

    Create shopping list component

--- shopping-app/src/App.tsx
+++ shopping-app/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Greeter from './Component/Greeter';
+import ShoppingList from './Component/ShoppingList';
 import './App.css';
 
 function App() {
+  const items = [
+    {id: 1, product: "Lemon", quantity: "3"},
+    {id: 2, product: "Chicken", quantity: "2"}
+  ]
+
   return (
-    <div className="App">
-      <Greeter person="Name"/>
+    <div >
+      <ShoppingList items={items}/>
     </div>
   );
 }
