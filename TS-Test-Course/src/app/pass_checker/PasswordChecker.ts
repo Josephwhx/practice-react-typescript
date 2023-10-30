c89741dd35f7927d279ca136504db0d9ff2ebe30
Josephwhx <JosephWonghx@gmail.com>
2023-10-30 12:34:40 +0800

    password checker 1st iteration

--- TS-Test-Course/src/app/pass_checker/PasswordChecker.ts
+++ TS-Test-Course/src/app/pass_checker/PasswordChecker.ts
@@ -2,7 +2,18 @@
 
 
 export class PasswordChecker {
-    public checkPassword() {
+
+    public checkPassword(password: string): boolean {
+        if(password.length < 8){
+            return false;
+        }
+        else if(password == password.toLowerCase()){
+            return false;
+        }
+        else if(password == password.toUpperCase()){
+            return false;
+        }
         
+        return true;
     }
 }
