3344a8a5516b0724f5f0d0da75dc54fdf80f2d29
Josephwhx <JosephWonghx@gmail.com>
2023-10-31 16:21:52 +0800

    Include remaining test cases for data access ts files

--- TS-Test-Course/src/test/server_app/data/ReservationDataAccess.test.ts
+++ TS-Test-Course/src/test/server_app/data/ReservationDataAccess.test.ts
@@ -1,48 +1,98 @@
-import { DataBase } from "../../../app/server_app/data/DataBase";
-import { ReservationsDataAccess } from "../../../app/server_app/data/ReservationsDataAccess";
+import { ReservationsDataAccess } from "../../../app/server_app/data/ReservationsDataAccess"
+import { DataBase } from '../../../app/server_app/data/DataBase';
+import * as IdGenerator from '../../../app/server_app/data/IdGenerator';
 import { Reservation } from "../../../app/server_app/model/ReservationModel";
 
-const insertMock = jest.fn();
-const getByMock = jest.fn();
+const mockInsert = jest.fn();
+const mockGetBy = jest.fn();
+const mockUpdate = jest.fn();
+const mockDelete = jest.fn();
+const mockGetAllElements = jest.fn();
 
-jest.mock('../../../app/server_app/data/DataBase', () => ({
-    DataBase: jest.fn().mockImplementation(() => ({
-        insert: insertMock,
-        getBy: getByMock
-    }))
-}));
+jest.mock('../../../app/server_app/data/DataBase', () => {
+    return {
+        DataBase: jest.fn().mockImplementation(() => {
+            return {
+                insert: mockInsert,
+                getBy: mockGetBy,
+                update: mockUpdate,
+                delete: mockDelete,
+                getAllElements: mockGetAllElements
+            }
+        })
+    }
+});
 
-
-describe('ReservationDataAccess test suite', () => {
+describe('ReservationsDataAccess test suite', () => {
 
     let sut: ReservationsDataAccess;
 
+    const someId = '1234';
+
     const someReservation: Reservation = {
+        endDate: 'someEndDate',
+        startDate: 'someStartDate',
         id: '',
         room: 'someRoom',
-        user: 'someUser',
-        startDate: 'someStartDate',
-        endDate: 'someEndDate'
+        user: 'someUser'
     }
-
-    const someId = '1234';
 
     beforeEach(() => {
         sut = new ReservationsDataAccess();
         expect(DataBase).toHaveBeenCalledTimes(1);
+        jest.spyOn(IdGenerator, 'generateRandomId').mockReturnValueOnce(someId);
     });
 
     afterEach(() => {
         jest.clearAllMocks();
+        someReservation.id = '';
+    })
+
+    it('should return the id of newly created reservation', async () => {
+        mockInsert.mockResolvedValueOnce(someId);
+
+        const actual = await sut.createReservation(someReservation);
+
+        expect(actual).toBe(someId);
+        expect(mockInsert).toBeCalledWith(someReservation);
     });
 
-    it('should add a reservation and return the id', async () => {
-        insertMock.mockResolvedValueOnce(someId);
+    it('should make the update reservation call', async () => {
+        await sut.updateReservation(
+            someId,
+            'endDate',
+            'someOtherEndDate'
+        )
 
-        const actualId = await sut.createReservation(someReservation);
-
-        expect(actualId).toBe(someId);
-        expect(insertMock).toHaveBeenCalledWith(someReservation);
+        expect(mockUpdate).toBeCalledWith(
+            someId,
+            'endDate',
+            'someOtherEndDate'
+        )
     });
 
-});
+    it('should make the delete reservation call', async () => {
+        await sut.deleteReservation(someId);
+
+        expect(mockDelete).toBeCalledWith(someId);
+    });
+
+    it('should return reservation by id', async () => {
+        mockGetBy.mockResolvedValueOnce(someReservation);
+
+        const actual = await sut.getReservation(someId);
+
+        expect(actual).toEqual(someReservation);
+        expect(mockGetBy).toBeCalledWith('id', someId);
+    });
+
+    it('should return all reservations', async () => {
+        mockGetAllElements.mockResolvedValueOnce([someReservation, someReservation]);
+
+        const actual = await sut.getAllReservations();
+
+        expect(actual).toEqual([someReservation, someReservation]);
+        expect(mockGetAllElements).toBeCalledTimes(1);
+    });
+    
+})
